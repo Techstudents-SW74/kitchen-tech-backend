83eece39113ec5f01b86fd06fc596cd5b8d8784a
Esteban <estebancalcha@gmail.com>
2024-10-19 18:52:29 -0500

    Add CreateTable

--- src/main/java/com/kitchenapp/kitchentech/iot/controller/TableController.java
+++ src/main/java/com/kitchenapp/kitchentech/iot/controller/TableController.java
@@ -1,17 +1,12 @@
 package com.kitchenapp.kitchentech.iot.controller;
 
-import com.kitchenapp.kitchentech.business.model.Product;
-import com.kitchenapp.kitchentech.business.service.ProductService;
 import com.kitchenapp.kitchentech.iot.model.Table;
 import com.kitchenapp.kitchentech.iot.service.TableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,5 +40,14 @@ public class TableController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(tableService.getTableById(tableId), HttpStatus.OK);
+    }
+
+    // URL: http://localhost:8080/api/kitchentech/v1/table
+    // Method: POST
+    @Transactional
+    @PostMapping
+    public ResponseEntity<Table> createTable(@RequestBody Table table) {
+        tableService.validateTable(table);
+        return new ResponseEntity<>(tableService.createTable(table), HttpStatus.CREATED);
     }
 }
